27e336d743d8f905bca8e76b844b50735877aad7
SithilSemitha <sithilsemithad@gmail.com>
2026-06-25 18:00:59 +0530

    feat(SCRUM-6): updated Auth Routes to match API Endpoints

---
+++ src/routes/AuthCallbackRoute.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+function AuthCallbackRoute() {
+  const location = useLocation();
+  const [redirectTo, setRedirectTo] = useState<string | null>(null);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    let accessToken = searchParams.get("access_token");
+
+    if (!accessToken) {
+      const hash = location.hash.startsWith("#") ? location.hash.slice(1) : location.hash;
+      const hashParams = new URLSearchParams(hash);
+      accessToken = hashParams.get("access_token");
+    }
+
+    if (accessToken) {
+      localStorage.setItem("deathbed.accessToken", accessToken);
+      setRedirectTo("/dashboard");
+    } else {
+      setRedirectTo("/login");
+    }
+  }, [location]);
+
+  if (!redirectTo) {
+    return null;
+  }
+
+  return <Navigate to={redirectTo} replace />;
+}
+
+export default AuthCallbackRoute;
