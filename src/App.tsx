7d5c27c51b93c19c442a4bcb126f2e8737a6d9e7
SithilSemitha <sithilsemithad@gmail.com>
2026-06-25 18:01:18 +0530

    feat: Updated App.tsx with route calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import OnboardingRoute from './routes/OnboardingRoute'
-import DecisionInputRoute from './routes/DecisionInputRoute'
-import DataUsageRoute from './routes/DataUsageRoute'
-import LoginRoute from './routes/LoginRoute'
-import ForgotPasswordRoute from './routes/ForgotPasswordRoute'
-import ResetPasswordRoute from './routes/ResetPasswordRoute'
-import DashboardRoute from './routes/DashboardRoute'
-import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import OnboardingRoute from "./routes/OnboardingRoute";
+import DecisionInputRoute from "./routes/DecisionInputRoute";
+import DataUsageRoute from "./routes/DataUsageRoute";
+import LoginRoute from "./routes/LoginRoute";
+import SignUpRoute from "./routes/SignUpRoute";
+import ForgotPasswordRoute from "./routes/ForgotPasswordRoute";
+import ResetPasswordRoute from "./routes/ResetPasswordRoute";
+import DashboardRoute from "./routes/DashboardRoute";
+import AuthCallbackRoute from "./routes/AuthCallbackRoute";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/onboarding" replace />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/auth/callback" element={<AuthCallbackRoute />} />
         <Route path="/onboarding" element={<OnboardingRoute />} />
+        <Route path="/signup" element={<SignUpRoute />} />
         <Route path="/data-usage" element={<DataUsageRoute />} />
         <Route path="/login" element={<LoginRoute />} />
         <Route path="/forgot-password" element={<ForgotPasswordRoute />} />
@@ -22,7 +26,7 @@ function App() {
         <Route path="/decisions/new" element={<DecisionInputRoute />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
